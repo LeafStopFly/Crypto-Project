8979bdbfc884b11f2f4ef2fcd1d4ee9c7a458d3c
Shelly <shelly.chao@iss.nthu.edu.tw>
2021-04-25 14:08:14 +0800

    create tables

---
+++ Rakefile.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'rake/testtask'
+
+task :default => :spec
+
+desc 'Tests API specs only'
+task :api_spec do
+  sh 'ruby spec/api_spec.rb'
+end
+
+desc 'Test all the specs'
+Rake::TestTask.new(:spec) do |t|
+  t.pattern = 'spec/*_spec.rb'
+  t.warning = false
+end
+
+desc 'Runs rubocop on tested code'
+task :style => [:spec, :audit] do
+  sh 'rubocop .'
+end
+
+desc 'Update vulnerabilities lit and audit gems'
+task :audit do
+  sh 'bundle audit check --update'
+end
+
+desc 'Checks for release'
+task :release? => [:spec, :style, :audit] do
+  puts "\nReady for release!"
+end
+
+task :print_env do
+  puts "Environment: #{ENV['RACK_ENV'] || 'development'}"
+end
+
+desc 'Run application console (pry)'
+task :console => :print_env do
+  sh 'pry -r ./spec/test_load_all'
+end
+
+namespace :db do
+  require_relative 'config/environments' # load config info
+  require 'sequel'
+
+  Sequel.extension :migration
+  app = Internship::Api
+
+  desc 'Run migrations'
+  task :migrate => :print_env do
+    puts 'Migrating database to latest'
+    Sequel::Migrator.run(app.DB, 'app/db/migrations')
+  end
+
+  desc 'Delete database'
+  task :delete do
+    app.DB[:documents].delete
+    app.DB[:projects].delete
+  end
+
+  desc 'Delete dev or test database file'
+  task :drop do
+    if app.environment == :production
+      puts 'Cannot wipe production database!'
+      return
+    end
+
+    db_filename = "app/db/store/#{Internship::Api.environment}.db"
+    FileUtils.rm(db_filename)
+    puts "Deleted #{db_filename}"
+  end
+
+  desc 'Delete and migrate again'
+  task reset: [:drop, :migrate]
+end
---
+++ config/environments.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'figaro'
+require 'sequel'
+
+module Internship
+  # Configuration for the API
+  class Api < Roda
+    plugin :environments
+
+    Figaro.application = Figaro::Application.new(
+      environment: environment,
+      path: File.expand_path('config/secrets.yml')
+    )
+    Figaro.load
+
+    # Make the environment variables accessible to other classes
+    def self.config
+      Figaro.env
+    end
+
+    DB = Sequel.connect(config.DATABASE_URL)
+
+    # Make the database accessible to other classes
+    def self.DB # rubocop:disable Naming/MethodName
+      DB
+    end
+
+    configure :development, :test do
+      require 'pry'
+    end
+  end
+end
