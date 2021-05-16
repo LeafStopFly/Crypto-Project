9f89d694a845b59641f80694cdaa9008dad43f93
Shelly <shelly.chao@iss.nthu.edu.tw>
2021-05-16 13:19:03 +0800

    modified internship and interview count

--- spec/integration/api_internships_spec.rb
+++ spec/integration/api_internships_spec.rb
@@ -23,7 +23,7 @@ describe 'Test Internship Handling' do
     _(last_response.status).must_equal 200
 
     result = JSON.parse last_response.body
-    _(result['data'].count).must_equal 2
+    _(result['data'].count).must_equal 3
   end
 
   it 'HAPPY: should be able to get details of a single internship' do
