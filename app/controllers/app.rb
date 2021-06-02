b684f7aafa9eef3549b15c81f703c5185306ce5e
Shelly <shelly.chao@iss.nthu.edu.tw>
2021-06-03 00:28:16 +0800

    Send email verification and require auth tokens on some routes

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,16 +2,16 @@
 
 require 'roda'
 require 'json'
+require_relative './helpers'
 
 module ISSInternship
   # Web controller for Internship API
   class Api < Roda
     plugin :halt
     plugin :multi_route
+    plugin :request_headers
 
-    def secure_request?(routing)
-      routing.scheme.casecmp(Api.config.SECURE_SCHEME).zero?
-    end
+    include SecureRequestHelpers
 
     route do |routing|
       response['Content-Type'] = 'application/json'
@@ -24,9 +24,11 @@ module ISSInternship
         { message: 'ISS InternshipAPI up at /api/v1' }.to_json
       end
 
-      @api_root = 'api/v1'
-      routing.on @api_root do
-        routing.multi_route
+      routing.on 'api' do
+        routing.on 'v1' do
+          @api_root = 'api/v1'
+          routing.multi_route
+        end
       end
     end
   end
